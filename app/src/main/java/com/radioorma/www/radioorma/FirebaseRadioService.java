3f3a8007f548949c76c4f8d264cecca23842c5ca
e.gioacchini <e.gioacchini@reply.it>
2018-05-29 18:23:57 +0200

    Prova commit

--- app/src/main/java/com/radioorma/www/radioorma/FirebaseRadioService.java
+++ app/src/main/java/com/radioorma/www/radioorma/FirebaseRadioService.java
@@ -1,14 +1,11 @@
 package com.radioorma.www.radioorma;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
-import android.util.Log;
+import android.app.Notification;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-
-import static android.content.ContentValues.TAG;
 
 public class FirebaseRadioService extends FirebaseMessagingService {
     public FirebaseRadioService() {
@@ -16,32 +13,14 @@ public class FirebaseRadioService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // ...
+        super.onMessageReceived(remoteMessage);
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .build();
+        NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
+        manager.notify(123, notification);
 
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-
-        // Check if message contains a data payload.
-        if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-                //TODO scheduleJob();
-            } else {
-                // Handle message within 10 seconds
-                //TODO handleNow();
-            }
-
-        }
-
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-        }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
     }
 }
--- app/src/main/java/com/radioorma/www/radioorma/MainActivity.java
+++ app/src/main/java/com/radioorma/www/radioorma/MainActivity.java
@@ -1,23 +1,56 @@
 package com.radioorma.www.radioorma;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView mWebView;
+    private FloatingActionButton mFloatingButton;
+
+    private boolean isActive = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//Todo prova
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        mFloatingButton = (FloatingActionButton) findViewById(R.id.floatingActionButton);
         // Enable Javascript
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         mWebView.loadUrl("http://www.radioorma.com/");
+        setupNotificationFloating();
     }
+
+    private void setupNotificationFloating() {
+
+        mFloatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isActive = !isActive;
+                if (isActive) {
+                    enableNotifications();
+                } else {
+                    disableNotifications();
+                }
+
+            }
+        });
+    }
+
+    private void enableNotifications() {
+        mFloatingButton.setImageResource(R.mipmap.ic_alert_on);
+        //TODO
+    }
+
+    private void disableNotifications() {
+        mFloatingButton.setImageResource(R.mipmap.ic_alert_off);
+        //TODO
+    }
+
 }
