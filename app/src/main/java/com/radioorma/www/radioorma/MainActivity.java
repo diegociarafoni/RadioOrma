6385c337c378cde902aabe92aecd651545937b16
e.gioacchini <e.gioacchini@reply.it>
2018-05-29 15:36:42 +0200

    Prova commit

--- app/src/main/java/com/radioorma/www/radioorma/MainActivity.java
+++ app/src/main/java/com/radioorma/www/radioorma/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //TODO ciao brutta bestia impara a fare i commit
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
